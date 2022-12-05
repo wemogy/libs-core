63bb27e603dc73ca65e1b5dcdaff9540c73f98af
quangtrang1111 <huynhquangtrang1994@gmail.com>
2022-12-05 09:44:38 +0700

    Add more extension AdaptValidateAndThrow to map existing instance.

--- src/Wemogy.Core/Mapster/Extensions/AdaptExtensions.cs
+++ src/Wemogy.Core/Mapster/Extensions/AdaptExtensions.cs
@@ -11,6 +11,17 @@ namespace Wemogy.Core.Mapster.Extensions
             var validator = new TFluentValidator();
             var mapped = source.Adapt<TDestination>();
             validator.ValidateAndThrow(mapped);
+
+            return mapped;
+        }
+
+        public static TDestination AdaptValidateAndThrow<TDestination, TFluentValidator>(this object source, TDestination destination)
+            where TFluentValidator : IValidator<TDestination>, new()
+        {
+            var validator = new TFluentValidator();
+            var mapped = source.Adapt(destination);
+            validator.ValidateAndThrow(mapped);
+
             return mapped;
         }
     }
