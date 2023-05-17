7936052dc475ee7622bb8af61a24c0aeb0e0fb7d
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2023-05-17 15:42:43 +0200

    Make Application Insights Optional in Monitoring Service

--- src/Wemogy.Core/Monitoring/MonitoringService.cs
+++ src/Wemogy.Core/Monitoring/MonitoringService.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.ApplicationInsights;
 using Prometheus;
 
@@ -5,19 +6,26 @@ namespace Wemogy.Core.Monitoring
 {
     public class MonitoringService : IMonitoringService
     {
-        readonly TelemetryClient _appInsightsTelemetryClient;
+        readonly TelemetryClient? _appInsightsTelemetryClient;
         readonly MonitoringEnvironment _env;
 
-        public MonitoringService(TelemetryClient appInsightsTelemetryClient, MonitoringEnvironment env)
+        public MonitoringService(MonitoringEnvironment env, IServiceProvider services)
         {
-            _appInsightsTelemetryClient = appInsightsTelemetryClient;
+            if (env.UseApplicationInsights)
+            {
+                // Don't get the TelemetryClient from the DI via constructor, as it
+                // might not be registered when ApplicationInsights is disabled. Instead, get it
+                // from the DI container here when it's actually needed.
+                _appInsightsTelemetryClient = (TelemetryClient)services.GetService(typeof(TelemetryClient));
+            }
+
             _env = env;
         }
 
         public void TrackEvent(string eventName, string eventDescription = "")
         {
             // Application Insights
-            if (_env.UseApplicationInsights)
+            if (_env.UseApplicationInsights && _appInsightsTelemetryClient != null)
             {
                 _appInsightsTelemetryClient.TrackEvent(eventName);
             }
