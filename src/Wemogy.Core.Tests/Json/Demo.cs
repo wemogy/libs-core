a6e487fce21518650dad64c02b8ea9fada9a10ec
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2023-11-25 16:47:15 +0100

    Add JSON Tests

---
+++ src/Wemogy.Core.Tests/Json/Demo.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Wemogy.Core.Tests;
+
+public class Demo
+{
+    [JsonInclude]
+    public int Id { get; private set; }
+}
---
+++ src/Wemogy.Core.Tests/Json/WemogyJsonTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using Wemogy.Core.Json;
+using Xunit;
+
+namespace Wemogy.Core.Tests;
+
+public class WemogyJsonTests
+{
+    [Fact]
+    public void PrivateSetters_GetSet()
+    {
+        // Arrange
+        var json = @"{""id"": 123}";
+
+        // Act
+        var demo = JsonSerializer.Deserialize<Demo>(json, WemogyJson.Options);
+
+        // Assert
+        Assert.Equal(123, demo!.Id);
+    }
+}
